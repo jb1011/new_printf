e07fcdd91718c04ffffc0d73bf6c922573d91327
jdemenet <jdemenet@student.42.fr>
2021-01-15 13:30:45 +0100

    all clean, all good

--- src/print_d.c
+++ src/print_d.c
@@ -14,16 +14,16 @@
 
 void	ft_print_d(t_arg *my_arg)
 {
-	long int	nb_d;
+	long int	lenght_d;
 	int			i;
 	int			j;
 
-	nb_d = ft_nbr_d_size(my_arg->d, my_arg);
-	i = my_arg->width - nb_d;
-	j = my_arg->accuracy - nb_d;
+	lenght_d = ft_nbr_d_size(my_arg->d, my_arg);
+	i = my_arg->width - lenght_d;
+	j = my_arg->accuracy - lenght_d;
 	my_arg->less = 'x';
-	if (my_arg->width <= nb_d || my_arg->accuracy < nb_d)
-		ft_case1_d(nb_d, i, j, my_arg);
+	if (my_arg->width <= lenght_d || my_arg->accuracy < lenght_d)
+		ft_case1_d(lenght_d, i, j, my_arg);
 	else
 	{
 		if (my_arg->width > my_arg->accuracy && my_arg->flag == '-')
@@ -35,9 +35,9 @@ void	ft_print_d(t_arg *my_arg)
 	}
 }
 
-int		ft_case1_d(long int nb_d, int i, int j, t_arg *my_arg)
+int		ft_case1_d(long int lenght_d, int i, int j, t_arg *my_arg)
 {
-	if (my_arg->width > nb_d && my_arg->flag == '-')
+	if (my_arg->width > lenght_d && my_arg->flag == '-')
 	{
 		if (my_arg->accuracy == 0 && my_arg->d == 0)
 			ft_putchar(' ', my_arg);
@@ -46,7 +46,7 @@ int		ft_case1_d(long int nb_d, int i, int j, t_arg *my_arg)
 		ft_putchar_end(i, ' ', my_arg);
 		return (0);
 	}
-	else if ((my_arg->width > nb_d && my_arg->flag == '0')\
+	else if ((my_arg->width > lenght_d && my_arg->flag == '0')\
 				&& (my_arg->accuracy < 0))
 	{
 		if (my_arg->d < 0)
@@ -56,15 +56,15 @@ int		ft_case1_d(long int nb_d, int i, int j, t_arg *my_arg)
 		}
 		ft_putchar_end(i, '0', my_arg);
 	}
-	else if (my_arg->width > nb_d && my_arg->flag != '-')
+	else if (my_arg->width > lenght_d && my_arg->flag != '-')
 		ft_putchar_end(i, ' ', my_arg);
-	ft_case5_d(nb_d, j, my_arg);
+	ft_case5_d(lenght_d, j, my_arg);
 	return (0);
 }
 
-int		ft_case5_d(long int nb_d, int j, t_arg *my_arg)
+int		ft_case5_d(long int lenght_d, int j, t_arg *my_arg)
 {
-	if (my_arg->accuracy >= nb_d)
+	if (my_arg->accuracy >= lenght_d)
 	{
 		if (my_arg->d < 0)
 		{
